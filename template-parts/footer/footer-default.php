b4e6a1cdd6d3796b8177030f9ebc32a533f5c31e
MS Shohan <mswebarts@gmail.com>
2021-10-17 18:35:00 +0600

    Footer default layout widget area layout only code spacing change

--- template-parts/footer/footer-default.php
+++ template-parts/footer/footer-default.php
@@ -5,44 +5,44 @@
 			<div class="wooe-space-above-footer"></div>
 			<footer class="footer wooe-footer-one">
 
-			<?php
-			if( $wooe_options['enable-widget-area'] ) {
-				$layout = $wooe_options['widget-area-layout'];
+				<?php
+				if( $wooe_options['enable-widget-area'] ) {
+					$layout = $wooe_options['widget-area-layout'];
 
-				switch( $layout ) {
-					case '6x6':
-						get_template_part("/template-parts/footer/widget-layout-6", "6" );
-						break;
-					case '3x3x3x3':
-						get_template_part("/template-parts/footer/widget-layout-3-3-3", "3" );
-						break;
-					case '4x4x4':
-						get_template_part("/template-parts/footer/widget-layout-4-4", "4" );
-						break;
-					case '4x2x2x4':
-						get_template_part("/template-parts/footer/widget-layout-4-2-2", "4" );
-						break;
-					case '6x3x3':
-						get_template_part("/template-parts/footer/widget-layout-6-3", "3" );
-						break;
-					case '3x3x6':
-						get_template_part("/template-parts/footer/widget-layout-3-3", "6" );
-						break;
-					case '3x6x3':
-						get_template_part("/template-parts/footer/widget-layout-3-6", "3" );
-						break;
-					case '4x8':
-						get_template_part("/template-parts/footer/widget-layout-4", "8" );
-						break;
-					case '8x4':
-						get_template_part("/template-parts/footer/widget-layout-8", "4" );
-						break;
-					default :
-						get_template_part("/template-parts/footer/widget-layout-3-3-3", "3" );
-						break;
+					switch( $layout ) {
+						case '6x6':
+							get_template_part("/template-parts/footer/widget-layout-6", "6" );
+							break;
+						case '3x3x3x3':
+							get_template_part("/template-parts/footer/widget-layout-3-3-3", "3" );
+							break;
+						case '4x4x4':
+							get_template_part("/template-parts/footer/widget-layout-4-4", "4" );
+							break;
+						case '4x2x2x4':
+							get_template_part("/template-parts/footer/widget-layout-4-2-2", "4" );
+							break;
+						case '6x3x3':
+							get_template_part("/template-parts/footer/widget-layout-6-3", "3" );
+							break;
+						case '3x3x6':
+							get_template_part("/template-parts/footer/widget-layout-3-3", "6" );
+							break;
+						case '3x6x3':
+							get_template_part("/template-parts/footer/widget-layout-3-6", "3" );
+							break;
+						case '4x8':
+							get_template_part("/template-parts/footer/widget-layout-4", "8" );
+							break;
+						case '8x4':
+							get_template_part("/template-parts/footer/widget-layout-8", "4" );
+							break;
+						default :
+							get_template_part("/template-parts/footer/widget-layout-3-3-3", "3" );
+							break;
+					}
 				}
-			}
-			?>
+				?>
 
 				<?php 
 					if( ( $wooe_options["enable-payment-icons"] == true ) && isset( $wooe_options['wooe-payment-icons'] ) ) {
